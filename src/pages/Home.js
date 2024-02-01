1bcacdfb123db563e7ab48893e2e49a25946c94a
Roberto Ramirez <robert.624@hotmail.com>
2024-02-01 11:11:36 -0500

    Add menu page with sections, images and descriptions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,26 +3,28 @@ import AmbianceImage from "../assets/images/ambiance.jpg";
 import WinePairingsImage from "../assets/images/wine-pairings.jpg";
 
 function getExploreList() {
-
     const exploreListItemsContent = [
         {
             name: "Signature Dishes",
-            description: " Immerse yourself in a selection of our chef's specialties, each designed to leave a lasting impression.",
+            description:
+                " Immerse yourself in a selection of our chef's specialties, each designed to leave a lasting impression.",
             image: SignatureDishesImage,
-            attribution: `<a href="https://www.freepik.es/foto-gratis/ensalada-verduras-tomate-pimientos-salteados-perejil-limon-aceite-oliva_7652118.htm#query=special%20dishes&position=2&from_view=search&track=ais&uuid=db5fc085-9f42-48b4-ad7e-176086b5efd7">Imagen de stockking</a> en Freepik`
+            attribution: `<a href="https://www.freepik.es/foto-gratis/ensalada-verduras-tomate-pimientos-salteados-perejil-limon-aceite-oliva_7652118.htm#query=special%20dishes&position=2&from_view=search&track=ais&uuid=db5fc085-9f42-48b4-ad7e-176086b5efd7">Imagen de stockking</a> en Freepik`,
         },
         {
             name: "Ambiance",
-            description: "Enjoy the perfect blend of modern sophistication and cozy charm in our restaurant setting.",
+            description:
+                "Enjoy the perfect blend of modern sophistication and cozy charm in our restaurant setting.",
             image: AmbianceImage,
-            attribution: `<a href="https://www.freepik.es/foto-gratis/mesa-mimbre-gris-sillas-cristal-gris_6384729.htm#query=restaurant%20ambiance&position=3&from_view=search&track=ais&uuid=7c3ffc62-f548-41a6-a12f-d5f70c3c633b">Imagen de KamranAydinov</a> en Freepik`
+            attribution: `<a href="https://www.freepik.es/foto-gratis/mesa-mimbre-gris-sillas-cristal-gris_6384729.htm#query=restaurant%20ambiance&position=3&from_view=search&track=ais&uuid=7c3ffc62-f548-41a6-a12f-d5f70c3c633b">Imagen de KamranAydinov</a> en Freepik`,
         },
         {
             name: "Wine Pairings",
-            description: "Elevate your dining experience with our exquisite wine pairings, each carefully selected to complement your meal.",
+            description:
+                "Elevate your dining experience with our exquisite wine pairings, each carefully selected to complement your meal.",
             image: WinePairingsImage,
             attribution: `Foto de <a href="https://unsplash.com/es/@brett_jordan?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Brett Jordan</a> en <a href="https://unsplash.com/es/fotos/botella-de-vino-negro-junto-a-copa-de-vino-transparente-sobre-mesa-de-madera-marron-ljwiMZOYNcc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-            `
+            `,
         },
     ];
 
@@ -50,7 +52,6 @@ function getExploreList() {
     });
 
     return exploreList;
-
 }
 
 function Home() {
@@ -77,6 +78,11 @@ function Home() {
     const exploreList = getExploreList();
     exploreContainer.appendChild(exploreTitle);
     exploreContainer.appendChild(exploreList);
+    const exploreMessage = document.createElement("p");
+    exploreMessage.classList.add("explore-message");
+    exploreMessage.textContent =
+        "Embark on a gastronomic journey with us, where every bite tells a story!";
+    exploreContainer.appendChild(exploreMessage);
 
     homeElement.appendChild(introContainer);
     homeElement.appendChild(exploreContainer);
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,7 +1,71 @@
+import menuData from "./menu.json";
+
+function getMenuItem(item) {
+    const menuItem = document.createElement("div");
+    menuItem.classList.add("menu-item");
+
+    const name = document.createElement("p");
+    name.classList.add("item-name");
+    name.textContent = item.name;
+    menuItem.appendChild(name);
+
+    const image = document.createElement("img");
+    image.src = item.imageSrc;
+    image.alt = item.name;
+    menuItem.appendChild(image);
+
+    const description = document.createElement("p");
+    description.classList.add("item-escription");
+    description.textContent = item.description;
+    menuItem.appendChild(description);
+
+    return menuItem;
+}
+
+function getMenuSection(name, items) {
+
+    const section = document.createElement("div");
+    section.classList.add("menu-section");
+
+    const title = document.createElement("h3");
+    title.textContent = name;
+    section.appendChild(title);
+
+    const sectionItems = document.createElement("div");
+    sectionItems.classList.add("section-items");
+    section.appendChild(sectionItems);
+
+    items.forEach((item) => {
+        const menuItem = getMenuItem(item);
+        sectionItems.appendChild(menuItem);
+    });
+
+    return section;
+
+}
+
 function Menu() {
     const menuElement = document.createElement("div");
     menuElement.innerHTML = "<h1>Menu</h1>";
     menuElement.id = "menu";
+
+
+    const subtitle = document.createElement("h2");
+    subtitle.textContent = "Culinary Masterpieces That Ignite the Senses";
+    menuElement.appendChild(subtitle);
+
+    const sections = document.createElement("div");
+    sections.classList.add("menu-sections");
+
+    menuData.forEach((section) => {
+        const menuSection = getMenuSection(section.name, section.items);
+        sections.appendChild(menuSection);
+    });
+
+    menuElement.appendChild(sections);
+
+
+
     return menuElement;
 }
 
